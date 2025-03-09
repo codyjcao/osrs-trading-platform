72c31d61eed450047efea2491f8ac43f0d4fc731
Cody Cao <codycao@Codys-Laptop.local>
2025-03-09 17:04:31 -0400

    create_new_master function which creates a new master file from scratch based on interval and user-customizable list of items

--- osrs_GE_data.py
+++ osrs_GE_data.py
@@ -32,7 +32,9 @@ df_index = pd.DataFrame((pd.read_html(html)[0])['Item.1'])
 df_index = df_index.rename({'Item.1':'name'},axis=1)
 
 df_index['id'] = df_index['name'].apply(lambda x:items_df.index[items_df['name'] == x][0])
-        
+
+
+
 '''
 data:
     item database - items_api library every week
@@ -51,33 +53,85 @@ functions:
 '''
 
 
-def call_http_historical_prices(item_id = 1,interval = '24h'):
+def call_http_historical_prices(item_id: int = 1,time_interval: str = '24h'):
     headers = {
         'User-Agent': 'Trading Strategy Platform - @funkeryiffic on Discord',
         'From': 'email@gmail.com'
     }
-    response = requests.get(f'https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep={interval}&id={item_id}',headers=headers)
+    response = requests.get(f'https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep={time_interval}&id={item_id}',headers=headers)
     stats = json.loads(response.text)
     return stats
 
 
-def create_price_df(item_id,interval='6h'):
-    jsonData = call_http_historical_prices(item_id,interval)
+def create_price_df(item_id: int,time_interval: str = '6h') -> Optional[pd.DataFrame]:
+    # Pull data from the runescape api
+    jsonData = call_http_historical_prices(item_id,time_interval)
+    
+    # Load the data into a dataframe
     data = pd.DataFrame(jsonData)
+    
+    # No data available
     if data.shape[0] == 0:
         raise HistoricalDataNotFound('This item id has no historical data available')
 
+    # Some missing data
     if data.shape[0] < 365:
         print(f'DataFrame has fewer than 365 rows; some data may be missing: {item_id}')
 
     data['index_id'] = np.arange(len(data))
     temp = data['data']
     res = pd.json_normalize(temp)
-    
     res['id'] = item_id
+    
     return res
 
 
+def create_new_master(time_interval: str = '6h',filepath: Optional[str] = None, items_list: Optional[list] = None, overwrite: bool = False):
+    # Set filepath if not specified
+    if filepath is None:
+        filepath = Path(f'Master Files/master_file_{time_interval}.csv')
+    else:
+        filepath = Path(filepath)
+        
+    if items_list is None:
+        items_list = df_index['id'].values
+    
+    # Check that our filepath doesn't already exist, exit if it does
+    if filepath.exists():
+        print(f'{filepath} already exists')
+        if not overwrite:
+            print(f'Set `overwrite=True` to overwrite')
+            return
+        print(f'Overwriting {filepath}')
+    else:
+        # Create folders if necessary based on the specified filepath
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+    
+    # To store all of the dataframes
+    df_list = []
+    
+    # Loop through the desired items list
+    for item_id in items_list:
+        try:
+            df_list.append(create_price_df(item_id, time_interval))
+        except Exception as e:
+            print(f"Skipping item {item_id} due to error: {e}")
+    
+    if not df_list:
+        print('No data was collected. Skipping file creation')
+        return
+    
+    # Combine all of the dataframes into one large dataframe
+    df_combined = pd.concat(df_list,ignore_index = True)
+    
+    df_combined.to_csv(filepath)
+    print(f'New master file saved to {filepath}')
+        
+
+        
+        
+
+'''
 def read_master_file_csv(time_interval: str = '6h', filepath: Optional[str] = None) -> Optional[pd.DataFrame]:
     # Define file path if not provided
     if filepath is None:
@@ -112,6 +166,4 @@ def read_master_file_csv(time_interval: str = '6h', filepath: Optional[str] = No
 
     # Return sorted DataFrame
     return df.sort_index()
-    
-
-
+'''    
